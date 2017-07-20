74ddd1c9241e278ba9833e7990e31ebed33df8b2
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-20 12:09:35 +0200

    Enable memory tracking

--- source/code/Core.h
+++ source/code/Core.h
@@ -1,3 +1,10 @@
 #pragma once
 
+#ifdef DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#define newp new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#else
 #define newp new
+#endif
--- source/code/main.cpp
+++ source/code/main.cpp
@@ -1,12 +1,17 @@
 #include "Application.h"
 #include "Core.h"
-
 int main()
 {
+#ifdef DEBUG 
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
 	Application* application = newp Application;
 	application->init();
 	application->run();
-	
+	application->destroy();
+
 	delete application;
+
 	return 0;
 }
