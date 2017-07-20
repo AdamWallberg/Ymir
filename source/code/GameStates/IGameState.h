079f48a6b5d2369616c3b13c6e36534c2a27f9bc
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-20 03:04:02 +0200

    Create interface for game states

---
+++ source/code/GameStates/IGameState.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class GameStateMachine;
+
+class IGameState
+{
+public:
+	IGameState(GameStateMachine* machine)
+		: machine_(machine)
+	{
+	}
+
+	virtual void onCreate() = 0;
+	virtual void onDestroy() = 0;
+	virtual void onEnter() = 0;
+	virtual void onExit() = 0;
+	virtual void update() = 0;
+protected:
+	GameStateMachine* machine_;
+};
