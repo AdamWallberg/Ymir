c36c6d5964b11b49507b53d923efbe52e1ffe022
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-20 03:05:15 +0200

    Make use of new application class

--- source/code/main.cpp
+++ source/code/main.cpp
@@ -1,28 +1,12 @@
-#include "Logger.h"
-#include "Graphics/Window.h"
+#include "Application.h"
+#include "Core.h"
 
 int main()
 {
-	Logger* logger = new Logger;
-	LoggerLocator::provide(logger);
-
-	Window* window = new Window;
-	WindowLocator::provide(window);
-
-	if (window->createWindow("Ymir Engine", 1280, 720))
-	{
-		while (!window->shouldClose())
-		{
-			window->update();
-			window->swapBuffers();
-		}
-	}
-
-	WindowLocator::provide(nullptr);
-	delete window;
-
-	LoggerLocator::provide(nullptr);
-	delete logger;
-
+	Application* application = newp Application;
+	application->init();
+	application->run();
+	
+	delete application;
 	return 0;
 }
