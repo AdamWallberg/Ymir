26536480fc40179695c9dafb23be4ab118fe98a4
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-22 23:20:39 +0200

    Implement methods for setting glsl uniforms

--- source/code/Graphics/Shader.cpp
+++ source/code/Graphics/Shader.cpp
@@ -117,3 +117,38 @@ uint Shader::loadShaderFromFile(std::string directory, std::string file_name, in
 
 	return id;
 }
+
+void Shader::setBool(const std::string& name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(id_, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value) const
+{
+	glUniform1i(glGetUniformLocation(id_, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const
+{
+	glUniform1f(glGetUniformLocation(id_, name.c_str()), value);
+}
+
+void Shader::setVec2(const std::string& name, pm::vec2 value) const
+{
+	glUniform2f(glGetUniformLocation(id_, name.c_str()), value.x, value.y);
+}
+
+void Shader::setVec3(const std::string& name, pm::vec3 value) const
+{
+	glUniform3f(glGetUniformLocation(id_, name.c_str()), value.x, value.y, value.z);
+}
+
+void Shader::setVec4(const std::string& name, pm::vec4 value) const
+{
+	glUniform4f(glGetUniformLocation(id_, name.c_str()), value.x, value.y, value.z, value.w);
+}
+
+void Shader::setMat4(const std::string& name, pm::mat4 value) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(id_, name.c_str()), 1, GL_FALSE, &value[0]);
+}
--- source/code/Graphics/Shader.h
+++ source/code/Graphics/Shader.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Types.h"
+#include "Math/vec2.h"
+#include "Math/vec3.h"
+#include "Math/vec4.h"
+#include "Math/mat4.h"
 
 class Shader
 {
@@ -10,14 +14,14 @@ public:
 	Shader(const std::string& vertex_name, const std::string& geometry_name, const std::string& fragment_name);
 	~Shader();
 	void bind();
-/*
+
 	void setBool(const std::string& name, bool value) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
-	void setVec2(const std::string& name, glm::vec2 value) const;
-	void setVec3(const std::string& name, glm::vec3 value) const;
-	void setVec4(const std::string& name, glm::vec4 value) const;
-	void setMat4(const std::string& name, glm::mat4 value) const;*/
+	void setVec2(const std::string& name, pm::vec2 value) const;
+	void setVec3(const std::string& name, pm::vec3 value) const;
+	void setVec4(const std::string& name, pm::vec4 value) const;
+	void setMat4(const std::string& name, pm::mat4 value) const;
 	uint getID() const { return id_; }
 private:
 	void setupProgram(std::string vertex_name, std::string geometry_name, std::string fragment_name);
